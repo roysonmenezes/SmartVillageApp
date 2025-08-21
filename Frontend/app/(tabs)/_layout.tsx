14e8c06cf2ce0582ffd392f993c8be8efea82e5a
roysonmenezes <royson32004@gmail.com>
2025-08-21 14:24:31 +0530

    removed extra bottom tabs

--- Frontend/app/(tabs)/_layout.tsx
+++ Frontend/app/(tabs)/_layout.tsx
@@ -1,76 +1,7 @@
-// import { Tabs } from 'expo-router';
-// import React from 'react';
-// import { Platform } from 'react-native';
-
-// import { HapticTab } from '@/components/HapticTab';
-// import { IconSymbol } from '@/components/ui/IconSymbol';
-// import TabBarBackground from '@/components/ui/TabBarBackground';
-// import { Colors } from '@/constants/Colors';
-// import { useColorScheme } from '@/hooks/useColorScheme';
-
-// export default function TabLayout() {
-//   const colorScheme = useColorScheme();
-
-//   return (
-//     <Tabs
-//       screenOptions={{
-//         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-//         headerShown: false,
-//         tabBarButton: HapticTab,
-//         tabBarBackground: TabBarBackground,
-//         tabBarStyle: Platform.select({
-//           ios: {
-//             // Use a transparent background on iOS to show the blur effect
-//             position: 'absolute',
-//           },
-//           default: {},
-//         }),
-//       }}>
-//       <Tabs.Screen
-//         name="index"
-//         options={{
-//           title: 'Home',
-//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="explore"
-//         options={{
-//           title: 'Explore',
-//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-//         }}
-//       />
-//       <Tabs.Screen
-//         name="BackendDataScreen"
-//         options={{
-//           title: 'Backend Data',
-//           tabBarIcon: ({ color }) => <IconSymbol size={28} name="server.rack" color={color} />, // Use an appropriate icon
-//         }}
-//       />
-//     </Tabs>
-//   );
-// }
-
-
-
-import { Tabs } from "expo-router";
+import { Stack } from "expo-router";
 
 export default function TabsLayout() {
   return (
-    <Tabs
-          screenOptions={{
-            headerShown: false,      // removes header bar at top
-            tabBarShowLabel: true,   // show custom titles
-          }}
-        >
-      <Tabs.Screen
-        name="admin/home"
-        options={{ title: "Admin Home"}}
-      />
-      <Tabs.Screen
-        name="villager/home"
-        options={{ title: "Villager Home" }}
-      />
-    </Tabs>
+    <Stack screenOptions={{ headerShown: false }} />
   );
 }

45ceba20bc6868eed5b250e39825d79f4741ce8a
roysonmenezes <royson32004@gmail.com>
2025-09-16 22:38:53 +0530

    resolving babel and version issues

--- Frontend/babel.config.js
+++ Frontend/babel.config.js
@@ -1,8 +1,14 @@
+
 // module.exports = function (api) {
 //   api.cache(true);
 //   return {
-//     presets: ["babel-preset-expo"],
-//     // plugins: ["nativewind/babel"],
+//     presets: [
+//       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+//       "nativewind/babel",
+//     ],
+//     plugins: [
+//       "react-native-reanimated/plugin", // Add this line - MUST be last!
+//     ],
 //   };
 // };
 
@@ -12,6 +18,14 @@ module.exports = function (api) {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+    ],
+    plugins: [
+      [
+        "react-native-reanimated/plugin",
+        {
+          globals: ["__scanWorklets"],
+        },
+      ],
     ],
   };
 };
